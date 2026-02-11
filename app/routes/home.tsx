eea824886922144b993ca23073f93d0073be8df3
Jordan Price <jprice@paramify.com>
2026-02-10 20:46:34 -0700

    Init shadcn

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,3 +1,4 @@
+import { Button } from '~/components/ui/button';
 import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
@@ -8,6 +9,7 @@ export default function Home() {
   return (
     <>
       <h1 className="text-2xl font-bold">Home</h1>
+      <Button>Click me</Button>
     </>
   );
 }
