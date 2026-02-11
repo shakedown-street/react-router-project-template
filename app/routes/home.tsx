ff96248df9a54d25b6d378469b5dbfded0916fb1
Jordan Price <jprice@paramify.com>
2026-02-10 19:54:57 -0700

    Init daisyui

--- app/routes/home.tsx
+++ app/routes/home.tsx
@@ -1,13 +1,13 @@
-import type { Route } from "./+types/home";
-import { Welcome } from "../welcome/welcome";
+import type { Route } from './+types/home';
 
 export function meta({}: Route.MetaArgs) {
-  return [
-    { title: "New React Router App" },
-    { name: "description", content: "Welcome to React Router!" },
-  ];
+  return [{ title: 'New React Router App' }, { name: 'description', content: 'Welcome to React Router!' }];
 }
 
 export default function Home() {
-  return <Welcome />;
+  return (
+    <div>
+      <button className="btn btn-primary">Click me</button>
+    </div>
+  );
 }
